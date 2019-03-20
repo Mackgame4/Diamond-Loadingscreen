a71053396ac014277dd14a79aabb9955cb628722
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2019-03-20 14:54:30 +0000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'Discord.png',
+    'Logo.png',
+    'logo.gif',
+    'video.mp4',
+    'audio.mp3
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
